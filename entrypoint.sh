8446dcaf0bc6287fc0d730995cd8a0e02014a189
Martin <martin@magextensions.io>
2021-01-04 03:01:38 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,16 @@ echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS & $INPUT_OVERRIDE_SETTINGS
 
 update-alternatives --set php /usr/bin/php${INPUT_PHP}
 
-if [ ${INPUT_OVERRIDE_SETTINGS} = 1 ]
-then
-  ls ./config/* && ls ./scripts/*
-  cp -rf ./config/* /opt/config/
-  cp -rf ./scripts/* /opt/scripts/
-  bash /opt/scripts/${INPUT_PROCESS}.sh
-else
-  bash /opt/scripts/${INPUT_PROCESS}.sh
-fi
+#if [ $INPUT_OVERRIDE_SETTINGS = 1 ]
+#then
+#  ls ./config/* && ls ./scripts/*
+#  cp -rf ./config/* /opt/config/
+#  cp -rf ./scripts/* /opt/scripts/
+#  bash /opt/scripts/${INPUT_PROCESS}.sh
+#else
+#  bash /opt/scripts/${INPUT_PROCESS}.sh
+#fi
+ls ./config/* && ls ./scripts/*
+cp -rf ./config/* /opt/config/
+cp -rf ./scripts/* /opt/scripts/
+bash /opt/scripts/${INPUT_PROCESS}.sh
