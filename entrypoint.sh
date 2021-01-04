67f37f8a739104e7e05e1ce18f155d0295e4557a
Martin <martin@magextensions.io>
2021-01-04 02:09:56 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "hello your setup is $INPUT_PHP & $INPUT_PROCESS & $INPUT_OVERRIDE_SETTINGS
 
 update-alternatives --set php /usr/bin/php${INPUT_PHP}
 
-if [ $INPUT_OVERRIDE_SETTINGS = 1 ]
+if [ ${INPUT_OVERRIDE_SETTINGS} = 1 ]
 then
   ls ./config/* && ls ./scripts/*
   cp -rf ./config/* /opt/config/
